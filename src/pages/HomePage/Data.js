6fe04b6042bde6df6683e1ca66b1eaf4ca6b8998
Tobi <tobi.daniel2018@gmail.com>
2021-05-22 10:04:20 -0400

    updated

--- src/pages/HomePage/Data.js
+++ src/pages/HomePage/Data.js
@@ -1,17 +1,16 @@
 export const homeObjOne = {
-  lightBg: false,
   primary: true,
   lightBg: false,
-  imgStart: "",
   lightTopLine: true,
-  lightTextDesc: true,
-  buttonLabel: "Get Started",
-  description:
-    "We help business owners increase their revenue. Our team of unique specialist can help you acheive your business goals.",
-  headline: "Lead Generation Specialist for Online Businesses",
   lightText: true,
+  lightTextDesc: true,
   topLine: "Marketing Agency",
-  img: "",
-  alt: "Image",
+  headline: "Lead Generation Specialist for Online Businesses",
+  description:
+    "We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.",
+  buttonLabel: "Get Started",
+  imgStart: "",
+  img: require("../../images/svg-1.svg").default,
+  alt: "Credit Card",
   start: "",
 };

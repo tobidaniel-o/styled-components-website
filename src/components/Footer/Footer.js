cbc3a044caf110c9a8f01aa1a77b785ce3dbe287
Tobi <tobi.daniel2018@gmail.com>
2021-05-22 13:50:49 -0400

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Button } from "../../globalStyles";
 import {
   FooterContainer,
   FooterSubscription,
   FooterSubHeading,
   FooterSubText,
+  Form,
+  FormInput,
+  FooterLinksContainer,
+  FooterLink,
+  FooterLinksItems,
+  FooterLinksWrapper,
+  FooterLinkTitle,
 } from "./Footer.elements";
 
 const Footer = () => {
@@ -14,7 +22,50 @@ const Footer = () => {
           Join our exclusive membership to receive the latest news and trends.
         </FooterSubHeading>
         <FooterSubText>You can unsubscribe at any time.</FooterSubText>
+        <Form>
+          <FormInput name="email" type="email" placeholder="Your Email" />
+          <Button fontBig>Subscribe</Button>
+        </Form>
       </FooterSubscription>
+      <FooterLinksContainer>
+        <FooterLinksWrapper>
+          <FooterLinksItems>
+            <FooterLinkTitle>About Us</FooterLinkTitle>
+            <FooterLink to="/sign-up">How it works</FooterLink>
+            <FooterLink to="/">Testimonials</FooterLink>
+            <FooterLink to="/">Careers</FooterLink>
+            <FooterLink to="/">Investors</FooterLink>
+            <FooterLink to="/">Terms of Service</FooterLink>
+          </FooterLinksItems>
+
+          <FooterLinksItems>
+            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+            <FooterLink to="/sign-up">How it works</FooterLink>
+            <FooterLink to="/">Testimonials</FooterLink>
+            <FooterLink to="/">Careers</FooterLink>
+            <FooterLink to="/">Investors</FooterLink>
+            <FooterLink to="/">Terms of Service</FooterLink>
+          </FooterLinksItems>
+
+          <FooterLinksItems>
+            <FooterLinkTitle>Videos</FooterLinkTitle>
+            <FooterLink to="/sign-up">How it works</FooterLink>
+            <FooterLink to="/">Testimonials</FooterLink>
+            <FooterLink to="/">Careers</FooterLink>
+            <FooterLink to="/">Investors</FooterLink>
+            <FooterLink to="/">Terms of Service</FooterLink>
+          </FooterLinksItems>
+
+          <FooterLinksItems>
+            <FooterLinkTitle>Social Media</FooterLinkTitle>
+            <FooterLink to="/sign-up">How it works</FooterLink>
+            <FooterLink to="/">Testimonials</FooterLink>
+            <FooterLink to="/">Careers</FooterLink>
+            <FooterLink to="/">Investors</FooterLink>
+            <FooterLink to="/">Terms of Service</FooterLink>
+          </FooterLinksItems>
+        </FooterLinksWrapper>
+      </FooterLinksContainer>
     </FooterContainer>
   );
 };

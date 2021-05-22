cbf090aadc32257de9e8e1ab98621e73124bce7f
Tobi <tobi.daniel2018@gmail.com>
2021-05-22 12:01:08 -0400

    updated

--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,13 +1,16 @@
-import React from 'react'
-import {homeObjOne} from './Data'
-import { InfoSection } from '../../components'
+import React from "react";
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from "./Data";
+import { InfoSection } from "../../components";
 
 const Home = () => {
   return (
     <>
       <InfoSection {...homeObjOne} />
+      <InfoSection {...homeObjTwo} />
+      <InfoSection {...homeObjThree} />
+      <InfoSection {...homeObjFour} />
     </>
   );
-}
+};
 
-export default Home
+export default Home;
